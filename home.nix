89eecb84aa1e43f40d5f225d75692e53c3386326
Lawrence Rabbets <lawrab@users.noreply.github.com>
2025-08-05 13:52:26 +0100

    Refactoring packages to seperate file

--- home.nix
+++ home.nix
@@ -5,37 +5,7 @@
   imports = [
     ./home/hyprland.nix
     ./home/waybar.nix
-  ];
-
-  home.stateVersion = "25.05";
-
-  home.packages = with pkgs; [
-    # ---- Your Existing Apps ----
-    firefox
-    kitty
-    wofi
-    waybar
-    vscode
-    git
-    
-    # -- Gaming Setup with Steam --
-    steam
-    protonup-qt
-    gamemode
-    gamescope
-    vulkan-tools
-
-    # ---- New App: Notification Daemon ----
-    mako
-    
-    # ---- Utilities ----
-    libnotify # For sending test notifications
-    pavucontrol # For audio control via Waybar
-    xfce.thunar
-
-    # ---- Fonts ----
-    # CORRECTED: Use the new nerd-fonts package for JetBrains Mono
-    nerd-fonts.jetbrains-mono
+    ./home/packages.nix
   ];
 
   programs.git = {
@@ -43,4 +13,6 @@
     userName = "Lawrence Rabbets";
     userEmail = "lrabbets@gmail.com";
   };
+
+  home.stateVersion = "25.05";
 }
---
+++ home/packages.nix
@@ -0,0 +1,38 @@
+{pkgs, ...}:
+
+{
+    home.packages = with pkgs; [
+    # ---- Your Existing Apps ----
+    firefox
+    kitty
+    wofi
+    waybar
+    vscode
+    git
+    
+    # -- Gaming Setup with Steam --
+    steam
+    protonup-qt
+    gamemode
+    gamescope
+    vulkan-tools
+
+    # ---- New App: Notification Daemon ----
+    mako
+    
+    # ---- Utilities ----
+    libnotify # For sending test notifications
+    pavucontrol # For audio control via Waybar
+    xfce.thunar
+
+    # ---- Fonts ----
+    # CORRECTED: Use the new nerd-fonts package for JetBrains Mono
+    nerd-fonts.jetbrains-mono
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "Lawrence Rabbets";
+    userEmail = "lrabbets@gmail.com";
+  };
+}
