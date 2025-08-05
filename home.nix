090705fa679b2c10258c64b7792b2b8cd92ac705
Lawrence Rabbets <lawrab@users.noreply.github.com>
2025-08-05 13:31:29 +0100

    Refactoring hyprland config to seperate file

--- home.nix
+++ home.nix
@@ -2,6 +2,10 @@
 { pkgs, ... }:
 
 {
+  imports = [
+    ./home/hyprland.nix
+  ];
+
   home.stateVersion = "25.05";
 
   home.packages = with pkgs; [
@@ -26,6 +30,7 @@
     # ---- Utilities ----
     libnotify # For sending test notifications
     pavucontrol # For audio control via Waybar
+    xfce.thunar
 
     # ---- Fonts ----
     # CORRECTED: Use the new nerd-fonts package for JetBrains Mono
@@ -36,76 +41,6 @@
     enable = true;
     userName = "Lawrence Rabbets";
     userEmail = "lrabbets@gmail.com";
-  };
-  
-  wayland.windowManager.hyprland = {
-    enable = true;
-    settings = {
-      monitor = ",preferred,auto,1";
-      
-      exec-once = [ 
-        "waybar" 
-        "mako"
-      ];
-
-      "$mainMod" = "SUPER";
-      
-      bind = [
-        "$mainMod, RETURN, exec, kitty"
-        "$mainMod, Q, killactive,"
-        "$mainMod, M, exit,"
-        "$mainMod, D, exec, wofi --show drun"
-        "$mainMod, left, movefocus, l"
-        "$mainMod, right, movefocus, r"
-        "$mainMod, up, movefocus, u"
-        "$mainMod, down, movefocus, d"
-        "$mainMod SHIFT, left, movewindow, l"
-        "$mainMod SHIFT, right, movewindow, r"
-        "$mainMod SHIFT, up, movewindow, u"
-        "$mainMod SHIFT, down, movewindow, d"
-        "$mainMod, 1, workspace, 1"
-        "$mainMod, 2, workspace, 2"
-        "$mainMod, 3, workspace, 3"
-        "$mainMod, 4, workspace, 4"
-        "$mainMod, 5, workspace, 5"
-        "$mainMod, 6, workspace, 6"
-        "$mainMod, 7, workspace, 7"
-        "$mainMod, 8, workspace, 8"
-        "$mainMod, 9, workspace, 9"
-        "$mainMod, 0, workspace, 10"
-        "$mainMod SHIFT, 1, movetoworkspace, 1"
-        "$mainMod SHIFT, 2, movetoworkspace, 2"
-        "$mainMod SHIFT, 3, movetoworkspace, 3"
-        "$mainMod SHIFT, 4, movetoworkspace, 4"
-        "$mainMod SHIFT, 5, movetoworkspace, 5"
-        "$mainMod SHIFT, 6, movetoworkspace, 6"
-        "$mainMod SHIFT, 7, movetoworkspace, 7"
-        "$mainMod SHIFT, 8, movetoworkspace, 8"
-        "$mainMod SHIFT, 9, movetoworkspace, 9"
-        "$mainMod SHIFT, 0, movetoworkspace, 10"
-      ];
-      
-      general = {
-        gaps_in = 5;
-        gaps_out = 10;
-        border_size = 2;
-        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-        "col.inactive_border" = "rgba(595959aa)";
-        allow_tearing = true;
-      };
-      decoration = { rounding = 10; };
-      animations = {
-        enabled = true; 
-        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
-        animation = [
-          "windows, 1, 7, myBezier"
-          "windowsOut, 1, 7, default, popin 80%"
-          "border, 1, 10, default"
-          "fade, 1, 7, default"
-          "workspaces, 1, 6, default"
-        ];
-      };
-    };
   };
 
   # Waybar configuration
---
+++ home/hyprland.nix
@@ -0,0 +1,103 @@
+{ pkgs, config, ...}:
+
+let
+  # Read the list of keybindings from your Hyprland config
+  keybinds = config.wayland.windowManager.hyprland.settings.bind;
+
+  # Function to format each keybinding line for display
+  format-keybind = bind:
+    let
+      # Split the line by comma, e.g., "$mainMod, RETURN, exec, kitty"
+      parts = pkgs.lib.splitString "," bind;
+      # Get the keys (first 2 parts), e.g., "$mainMod, RETURN"
+      keys = pkgs.lib.concatStringsSep "," (pkgs.lib.take 2 parts);
+      # Get the action (the rest), e.g., "exec, kitty"
+      action = pkgs.lib.concatStringsSep "," (pkgs.lib.drop 2 parts);
+    in
+    # Replace variables and format for readability
+    "<b>${pkgs.lib.replaceStrings [ "$mainMod" "SHIFT" ] [ "SUPER" "+Shift" ] keys}</b>: ${action}";
+
+  # Create the final list of formatted keybindings
+  formatted-keybinds = pkgs.lib.mapAttrsToList (name: value: format-keybind value) keybinds;
+
+  # Create a script package named "hypr-keybinds"
+  keybinds-script = pkgs.writeShellScriptBin "hypr-keybinds" ''
+    echo -e "${pkgs.lib.concatStringsSep "\\n" formatted-keybinds}" | wofi --show dmenu -p "Hyprland Keybindings"
+  '';
+
+in # This is the end of the 'let' block and the start of your main config
+
+{
+    wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      monitor = ",preferred,auto,1";
+      
+      exec-once = [ 
+        "waybar" 
+        "mako"
+      ];
+
+      "$mainMod" = "SUPER";
+      
+      bind = [
+        "$mainMod, RETURN, exec, kitty"
+        "$mainMod, Q, killactive,"
+        "$mainMod, M, exit,"
+        "$mainMod, D, exec, wofi --show drun"
+        "$mainMod, E, exec, thunar"
+        "$mainMod, left, movefocus, l"
+        "$mainMod, right, movefocus, r"
+        "$mainMod, up, movefocus, u"
+        "$mainMod, down, movefocus, d"
+        "$mainMod SHIFT, left, movewindow, l"
+        "$mainMod SHIFT, right, movewindow, r"
+        "$mainMod SHIFT, up, movewindow, u"
+        "$mainMod SHIFT, down, movewindow, d"
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
+        "$mainMod SHIFT, 1, movetoworkspace, 1"
+        "$mainMod SHIFT, 2, movetoworkspace, 2"
+        "$mainMod SHIFT, 3, movetoworkspace, 3"
+        "$mainMod SHIFT, 4, movetoworkspace, 4"
+        "$mainMod SHIFT, 5, movetoworkspace, 5"
+        "$mainMod SHIFT, 6, movetoworkspace, 6"
+        "$mainMod SHIFT, 7, movetoworkspace, 7"
+        "$mainMod SHIFT, 8, movetoworkspace, 8"
+        "$mainMod SHIFT, 9, movetoworkspace, 9"
+        "$mainMod SHIFT, 0, movetoworkspace, 10"
+      ];
+      
+      general = {
+        gaps_in = 5;
+        gaps_out = 10;
+        border_size = 2;
+        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.inactive_border" = "rgba(595959aa)";
+        allow_tearing = true;
+      };
+      decoration = { rounding = 10; };
+      animations = {
+        enabled = true; 
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+        animation = [
+          "windows, 1, 7, myBezier"
+          "windowsOut, 1, 7, default, popin 80%"
+          "border, 1, 10, default"
+          "fade, 1, 7, default"
+          "workspaces, 1, 6, default"
+        ];
+      };
+    };
+  };
+
+  home.packages = [ keybinds-script ];
+}
