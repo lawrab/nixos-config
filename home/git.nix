f512c6ebac01032200af0c2f1e1ea0c40e456c51
Lawrence Rabbets <lawrab@users.noreply.github.com>
2025-08-09 23:19:54 +0100

    Add comprehensive git configuration with aliases and improved settings
    - Add pretty log aliases (lg, lga, ll) for better commit visualisation
    - Configure modern git defaults (main branch, rebase on pull, auto-setup remote)
    - Add common operation shortcuts (co, br, ci, st, df, dc)
    - Enable rerere for conflict resolution reuse
    - Improve diff and merge conflict display

--- home/git.nix
+++ home/git.nix
@@ -9,6 +9,47 @@
     # Set Neovim as the editor specifically for Git
     extraConfig = {
       core.editor = "nvim";
+      init.defaultBranch = "main";
+      pull.rebase = true;
+      push.autoSetupRemote = true;
+      diff.colorMoved = "default";
+      merge.conflictStyle = "diff3";
+      rerere.enabled = true;
+    };
+
+    # Git aliases for common operations
+    aliases = {
+      # Pretty log formats
+      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      lga = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all";
+      ll = "log --pretty=format:'%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]' --decorate --numstat";
+      
+      # Short status
+      st = "status -s";
+      
+      # Common operations
+      co = "checkout";
+      br = "branch";
+      ci = "commit";
+      ca = "commit -a";
+      cm = "commit -m";
+      cam = "commit -am";
+      
+      # Diff shortcuts
+      df = "diff";
+      dc = "diff --cached";
+      
+      # Reset shortcuts
+      unstage = "reset HEAD --";
+      last = "log -1 HEAD";
+      
+      # Show what happened
+      visual = "!gitk";
+      
+      # Stash shortcuts
+      sl = "stash list";
+      sa = "stash apply";
+      ss = "stash save";
     };
   };
 }
